e25875c39bfbad25afc865ed637ce65a7e9261a5
Islam Shagaew <you@example.com>
2022-04-20 16:00:56 +0300

    add new changes

--- users/query.py
+++ users/query.py
@@ -9,11 +9,18 @@ class CustomUserType(DjangoObjectType):
         fields = "__all__"
 
 
+def Q():
+    pass
+
+
 class Query(graphene.ObjectType):
     all_users = graphene.List(CustomUserType)
+    search_by_value = graphene.List(CustomUserType, value=graphene.String(required=True))
 
     def resolve_all_users(root, info):
         # We can easily optimize query count in the resolve method
         return CustomUser.objects.all()
 
-
+    def resolve_search_by_value(root, info, value):
+        users = CustomUser.objects.filter(Q(first_name__startswith=value)| Q(last_name__startswith=value)|Q(email__startswith=value)|Q(username__startswith=value))
+        return users
